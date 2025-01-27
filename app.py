b8bfde5b17009a3bea64492c5fb83149f81bd0d9
scoobSRW <scoob.ray.white@gmail.com>
2025-01-27 12:40:59 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+def calculate_sum(num1, num2):
+    return num1 + num2
+
+@app.route('/sum', methods=['POST'])
+def sum_route():
+    data = request.get_json()
+    num1 = data['num1']
+    num2 = data['num2']
+    result = calculate_sum(num1, num2)
+    return jsonify({'result': result})
